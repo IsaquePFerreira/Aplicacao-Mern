2523679371e3352816fa7d5ba1d25a056c000ea6
IsaquePFerreira <pferreiraisaque@gmail.com>
2025-12-05 19:59:37 -0300

    Create mongodb connect code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import "dotenv/config.js";
+import mongoose from "mongoose";
 
 const app = express();
+function connectToDabase() {
+    try {
+        mongoose.connect(process.env, MONGO_URI);
+    } catch () {}
+}
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000. CTRL + C stop.");
