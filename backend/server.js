7e6ca733b3ef29e42e9314e3fc546f09c0ac9cd9
IsaquePFerreira <pferreiraisaque@gmail.com>
2025-12-12 20:42:28 -0300

    Create app.post

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import "dotenv/config.js";
-import mongoose from "mongoose";
+import { connectToDabase } from "./config/db.js";
+import { Book } from "./book.model.js";
 
 const app = express();
-function connectToDabase() {
+
+app.use(express.json());
+
+app.post("/api/v1/books", async(req, res) => {
+    const {title, subtitle, author, genrer, cover} = req.body;
+
     try {
-        mongoose.connect(process.env, MONGO_URI);
-    } catch () {}
-}
+        const book = new Book({title, subtitle, author, genrer, cover});
+        await book.save();
+        res.status(201).json({sucess: true, data: book});
+    } catch(error) {
+        console.error("Error saving book", error);
+        res.status(500).json({sucess: false, error: "Error while save the book"})
+    }
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000. CTRL + C stop.");
+    connectToDabase();
 });
