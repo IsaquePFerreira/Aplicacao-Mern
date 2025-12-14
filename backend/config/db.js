c1739fdb9e4a970bbd55702c9e3a3e6a9c11c8d1
IsaquePFerreira <pferreiraisaque@gmail.com>
2025-12-13 21:29:46 -0300

    Add await instruction

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToDabase() {
     try {
-        // await mongoose.connect(process.env.MONGO_URI);
-        mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connect to the database");
     } catch (error) {
         console.error("Error connecting database...", error);
